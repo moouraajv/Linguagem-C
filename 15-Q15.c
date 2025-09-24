f59eede504741f466c29474089dbeace0b0e5c26
João Victor Moura <130857332+moouraajv@users.noreply.github.com>
2025-09-24 00:23:12 -0300

    Rename Q15.c to 15-Q15.c

--- 15-Q15.c
+++ 15-Q15.c
@@ -9,19 +9,19 @@ int main()
     int valor;
     char resto;
 
-    printf("Digitem um valor inteiro v·lido: ");
+    printf("Digitem um valor inteiro v√°lido: ");
     if (scanf("%d%c", &valor, &resto) != 2 || resto != '\n')
     {
-        printf("VocÍ n„o digitou um valor inteiro v·lido, tente novamente.\n");
+        printf("Voc√™ n√£o digitou um valor inteiro v√°lido, tente novamente.\n");
         return 1;
     }
     
     if (valor % 2 == 0)
     {
-        printf("O valor digitado È par. \n");
+        printf("O valor digitado √© par. \n");
     } else
     {
-        printf("O valor digitado È impar. \n");
+        printf("O valor digitado √© impar. \n");
     }
     
     system("pause");
