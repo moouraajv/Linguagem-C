c9f87a8f83bcbd5416e49cf6e41d3c58a197d09e
João Victor Moura <130857332+moouraajv@users.noreply.github.com>
2025-09-24 00:23:36 -0300

    Rename Q17.c to 17-Q17.c

--- 17-Q17.c
+++ 17-Q17.c
@@ -9,24 +9,24 @@ int main()
     int valor1, valor2;
     char resto;
 
-    printf("Digite 2 valores v·lidos abaixo. \n");
+    printf("Digite 2 valores v√°lidos abaixo. \n");
     if (scanf("%d%d%c", &valor1, &valor2, &resto) != 3 || resto != '\n')
     {
-        printf("VocÍ digitou um valor inv·lido, tente novamente. \n");
+        printf("Voc√™ digitou um valor inv√°lido, tente novamente. \n");
         return 1;
     }
 
     if (valor1 == valor2)
     {
-        printf("VocÍ digitou 2 valores iguais. \n");
+        printf("Voc√™ digitou 2 valores iguais. \n");
     } else
     {
         if (valor1 > valor2)
         {
-            printf("Os valores digitados s„o diferentes e %d È maior que %d. \n", valor1, valor2);
+            printf("Os valores digitados s√£o diferentes e %d √© maior que %d. \n", valor1, valor2);
         } else
         {
-            printf("Os valores digitados s„o diferentes e %d È maior que %d. \n", valor2, valor1);
+            printf("Os valores digitados s√£o diferentes e %d √© maior que %d. \n", valor2, valor1);
         }
            
     }
