78492867c79088dbe0e8c85580c3d2036e321009
João Victor Moura <130857332+moouraajv@users.noreply.github.com>
2025-09-24 00:22:03 -0300

    Rename Q10.c to 10-Q10.c

--- 10-Q10.c
+++ 10-Q10.c
@@ -12,38 +12,38 @@ int main()
     scanf("%d", &valor_inteiro);
 
     printf("Valor a ser sacado: R$ %d,00\n", valor_inteiro);
-    printf("Quantidade de cédulas que você receberá: \n");
+    printf("Quantidade de cÃ©dulas que vocÃª receberÃ¡: \n");
 
     int valor_restante = valor_inteiro;
 
     int quantidade;
 
     quantidade = valor_restante / 200;
-        printf("%d cédula(s) de R$ 200 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 200 \n", quantidade);
     valor_restante %= 200;
 
     quantidade = valor_restante / 100;
-        printf("%d cédula(s) de R$ 100 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 100 \n", quantidade);
     valor_restante %= 100;
 
     quantidade = valor_restante / 50;
-        printf("%d cédula(s) de R$ 50 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 50 \n", quantidade);
     valor_restante %= 50;
 
     quantidade = valor_restante / 20;
-        printf("%d cédula(s) de R$ 20 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 20 \n", quantidade);
     valor_restante %= 20;
 
     quantidade = valor_restante / 10;
-        printf("%d cédula(s) de R$ 10 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 10 \n", quantidade);
     valor_restante %= 10;
 
     quantidade = valor_restante / 5;
-        printf("%d cédula(s) de R$ 5 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 5 \n", quantidade);
     valor_restante %= 5;
 
     quantidade = valor_restante / 2;
-        printf("%d cédula(s) de R$ 2 \n", quantidade);
+        printf("%d cÃ©dula(s) de R$ 2 \n", quantidade);
     valor_restante %= 2;
 
     system("pause");
